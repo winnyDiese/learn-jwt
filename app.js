52d409a7367f2688e62a71e8b5fdad01b8c74b29
winnyDiese <winnydiese@gmail.com>
2022-08-17 10:55:36 +0200

    All code generate Token

---
+++ app.js
@@ -0,0 +1,76 @@
+
+// Import
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+// Config
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+// Create a user
+const user = {
+    id: 42,
+    name: 'Jambon Khen',
+    email: 'jambon@gmail.com',
+    admi: true
+}
+
+
+// creation func for generate a token
+const generateAccessToken = user =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1800s'})
+}
+
+
+// Route Login
+app.post('/api/login/', (req, res)=>{
+
+    // Todo : checker en base le user, par rapport a email
+    if(req.body.email != user.email){
+        res.status(401).send('Invalid email')
+        return
+    }
+    
+    if(req.body.password != 'cueilleur'){
+        res.status(401).send('Invalid pa<ssword')
+        return
+    }
+
+    // Generate a token
+    const accessToken = generateAccessToken(user)
+    res.send({accessToken})
+
+})
+
+
+// Verification de l'authentification du TOKEN
+const authenticateToken = (req, res, next)=>{
+    const authorization = req.header('authorization')
+    const token = authorization && authorization.split(' ')[1] // 'Berear token'
+
+    if(!token){
+        return res.sendStatus(401)
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if(err) return res.sendStatus(401)
+
+        req.user = user
+        next()
+    })
+
+}
+
+// Routes me
+app.get('/api/me', authenticateToken, (req, res)=>{
+    res.send(req.user)
+})
+
+app.listen(3000, ()=>{console.log('Server running port 3000 ')})
+
+
+
